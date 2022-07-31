016e30a3ba6e79ca6ab1152910dab2d278fe7d94
Dusty McCord <Dustymccord@gmail.com>
2022-07-31 14:57:08 -0700

    remove poker board

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -8,38 +8,38 @@ import { useDocument } from "react-firebase-hooks/firestore"
 import { useRouter } from "next/router"
 
 const Poker = () => {
-  const router = useRouter()
-  const { query } = router
-  const firebaseApp = getFirestore()
+  // const router = useRouter()
+  // const { query } = router
+  // const firebaseApp = getFirestore()
 
-  const [roomData, loading, error] = useDocument(
-    doc(firebaseApp, "rooms", `${query.id}`)
-  )
+  // const [roomData, loading, error] = useDocument(
+  //   doc(firebaseApp, "rooms", `${query.id}`)
+  // )
 
-  const roomDataReal = roomData?.data()
+  // const roomDataReal = roomData?.data()
 
-  const makeNewRoom = async () => {
-    const newRoom: Room = {
-      name: query.id as string,
-      isVoting: true,
-      users: [],
-    }
-    try {
-      await setDoc(doc(firebaseApp, "rooms", query.id as string), newRoom)
-    } catch (e) {
-      console.error("Error adding document: ", e)
-    }
-  }
+  // const makeNewRoom = async () => {
+  //   const newRoom: Room = {
+  //     name: query.id as string,
+  //     isVoting: true,
+  //     users: [],
+  //   }
+  //   try {
+  //     await setDoc(doc(firebaseApp, "rooms", query.id as string), newRoom)
+  //   } catch (e) {
+  //     console.error("Error adding document: ", e)
+  //   }
+  // }
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const makeNewRoomMemo = useCallback(() => makeNewRoom(), [query.id])
+  // // eslint-disable-next-line react-hooks/exhaustive-deps
+  // const makeNewRoomMemo = useCallback(() => makeNewRoom(), [query.id])
 
-  // Create room if room data return undefined
-  useEffect(() => {
-    if (!roomDataReal && !loading && !error) {
-      makeNewRoomMemo()
-    }
-  }, [error, loading, makeNewRoomMemo, roomDataReal])
+  // // Create room if room data return undefined
+  // useEffect(() => {
+  //   if (!roomDataReal && !loading && !error) {
+  //     makeNewRoomMemo()
+  //   }
+  // }, [error, loading, makeNewRoomMemo, roomDataReal])
 
   return (
     <div>
